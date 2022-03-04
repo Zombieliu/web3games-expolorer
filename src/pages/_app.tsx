b73219f261920b88281136145a9ebbefc9becba1
henryliu <17600047539@163.com>
2022-03-04 17:09:29 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import {createClient} from "graphql-ws"
 
 const client = new GraphQLClient({
     // url: 'https://api.subquery.network/sq/Zombieliu/devnet-webe3games',
-    url: 'https://api.subquery.network/sq/Zombieliu/devnet-webe3games',
+    url: 'https://api.subquery.network/sq/Zombieliu/web3games-dev-net',
     // cache:memCache(),
     // subscriptionClient:()=>
     //   createClient({
