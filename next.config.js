9cefcaca7458ae6b23b82bbf28c4af320d5d24e4
henryliu <17600047539@163.com>
2022-03-18 02:00:55 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const { i18n } = require('./next-i18next.config');
 
 module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/home',
+                destination: '/',
+            },
+        ]
+    },
     eslint: {
-        dirs: ['src/home'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+        dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     },
     i18n,
 }
