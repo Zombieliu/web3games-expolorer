c7de7019133c1570a13d4f606d37a28d65310bcb
henryliu <17600047539@163.com>
2022-12-21 19:53:33 +0800

    update

--- src/chain/web3games/index.ts
+++ src/chain/web3games/index.ts
@@ -1,5 +1,3 @@
-import {cropData} from "../../utils/math";
-import axios from "axios";
 import {CHAIN_URL,CHAIN_RPC_URL} from "../../constant";
 import {ApiPromise, WsProvider} from "@polkadot/api";
 
@@ -105,40 +103,6 @@ const chain_api = async ()=>{
   });
   return api
 }
-
-const substrate_wallet_injector = async (intactWalletAddress:string)=>{
-  const web3FromAddress = (await import("@polkadot/extension-dapp")).web3FromAddress;
-  const injector = await web3FromAddress(intactWalletAddress);
-  return injector
-}
-
-
-
-
-// const swap = async (intactWalletAddress)=>{
-//   const web3Enable = (await import("@polkadot/extension-dapp")).web3Enable;
-//   await web3Enable('my cool dapp');
-//   const web3FromAddress = (await import("@polkadot/extension-dapp")).web3FromAddress;
-//   const injector = await web3FromAddress(intactWalletAddress);
-//   const api = await chain_api(intactWalletAddress)
-//   const transferExtrinsic = api.tx.exchange.swapExactTokensForTokens(,10000,[1,2],"5CcgM2vkikJv6utQaS6jWDhV6DgnoyacKE81qzXZ52FSxkY8")
-//   transferExtrinsic.signAndSend(intactWalletAddress, { signer: injector.signer }, ({ status }) => {
-//     if (status.isInBlock) {
-//       console.log(`Completed at block hash #${status.asInBlock.toString()}`);
-//     } else {
-//       console.log(`Current status: ${status.type}`);
-//     }
-//   }).catch((error: any) => {
-//     console.log(':( transaction failed', error);
-//   });
-// }
-
-
-
-
 export{
   chain_api,
-  substrate_wallet_injector,
-  // swap,
-
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,12 @@ import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
 import "../css/font-awesome.css"
 import { appWithTranslation } from 'next-i18next';
-import { GraphQLClient, ClientContext } from 'graphql-hooks'
-import { useGraphQLClient } from '../graphql/graphql-client'
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const graphQLClient = useGraphQLClient(pageProps.initialGraphQLState)
-  return (
-      <ClientContext.Provider value={graphQLClient}>
-          <Component {...pageProps} />
-      </ClientContext.Provider>
-  )
+  return <Component {...pageProps} />
+
 }
 
 // export default MyApp
